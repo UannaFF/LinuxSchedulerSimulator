c3a9bd1ba1ebc1311808b630ecc01f712fc50261
Gabriel Russo <gabriel@localhost.localdomain>
2015-05-13 17:18:02 -0430

    ya se agregan los procesos en orden de llegada a la cola general de procesos

---
+++ src/Proceso.java
@@ -0,0 +1,44 @@
+/*
+*	Clase ::	Proceso
+*	Descripcion :: Clase que representa la estructura de datos de un 
+*				   proceso
+*	Estado :: Incompleto
+*/
+import java.util.ArrayList;
+
+
+public class Proceso {
+
+	public int pid = 0;
+	public String type = "";
+	public int arrivalTime = 0;
+	public int priority = 0;
+	public ArrayList<Pair<String,Integer>> resources = null;
+
+	public Proceso(int pid, String s, int prior, int arriveT, ArrayList<Pair<String,Integer>> pAr){
+
+		this.pid = pid;
+		this.type = s;
+		this.arrivalTime = arriveT;
+		this.priority = prior;
+		this.resources = pAr;
+		
+	}
+
+	public Pair<String,Integer> getFirstSource(){
+		if (!resources.isEmpty()){
+			return resources.get(0);
+		}
+		return null;
+	}
+
+	public void removeFirstSource(){
+		if (!resources.isEmpty()){
+			resources.remove(0);
+		}
+	}
+
+	public String toString(){
+		return "( " + this.pid + ", arrivo=" + this.arrivalTime+" , prioridad="+ this.priority +")"; 
+	}
+}
